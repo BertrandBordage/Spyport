47944610cdb41b831958911f5dfe1cfacd16074e
Bertrand Bordage <bordage.bertrand@gmail.com>
2026-04-04 18:25:15 +0200

    Fixes the embarkment.

--- scenes/spawner.gd
+++ scenes/spawner.gd
@@ -4,40 +4,28 @@ extends Node2D
 const character_scene := preload("res://scenes/character.tscn")
 const trolley_scene := preload("res://scenes/obstacles/trolley.tscn")
 
-@onready var physics_state := get_world_2d().direct_space_state
-var shape_params := PhysicsShapeQueryParameters2D.new()
-
-
-func spawn_child_in_empty_space(
-	instance: PhysicsBody2D, collision_shape: CollisionShape2D,
-) -> void:
-	shape_params.shape = collision_shape.shape
-	while true:
-		instance.position = Globals.get_random_position()
-		shape_params.transform = collision_shape.transform.translated(instance.position)
-		if physics_state.intersect_shape(shape_params, 1).size() == 0:
-			add_child.call_deferred(instance)
-			break
-
 
 func _ready() -> void:
+	Globals.spawner = self
+	Globals.physics_state = get_world_2d().direct_space_state
+	
 	for _i in range(300):
 		var character: Character = character_scene.instantiate()
 		character.player_index = Character.PlayerIndex.BOT
 		var collision_shape := character.get_collision_shape()
-		spawn_child_in_empty_space(character, collision_shape)
+		Globals.spawn_child_in_empty_space(character, collision_shape)
 
 	for i in range(20):
 		var trolley: Trolley = trolley_scene.instantiate()
 		var collision_shape := trolley.get_collision_shape()
-		spawn_child_in_empty_space(trolley, collision_shape)
+		Globals.spawn_child_in_empty_space(trolley, collision_shape)
 
 
 static func is_available_character(node: Node):
 	var taken_character_types: Array[CharacterType] = []
 	for character in Globals.players_characters.values():
 		taken_character_types.append(character.character_type)
-	return node is Character and node.character_type not in taken_character_types
+	return is_instance_valid(node) and node is Character and node.character_type not in taken_character_types
 
 
 func _unhandled_input(event: InputEvent) -> void:
