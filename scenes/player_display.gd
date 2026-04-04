8eb6f34e9790538dd00b6fa6f916b81c752d1da2
Bertrand Bordage <bordage.bertrand@gmail.com>
2026-04-04 16:03:56 +0200

    Implements the new player display.

--- scenes/player_display.gd
+++ scenes/player_display.gd
@@ -9,15 +9,30 @@ var character: Character
 func _ready() -> void:
 	Globals.player_joined.connect(_on_player_joined)
 	Globals.player_died.connect(_on_player_died)
-	%Label.text = "P%d" % (player_index + 1)
+	%Name.texture = {
+		Character.PlayerIndex.ONE: preload("res://assets/ui/player1.png"),
+		Character.PlayerIndex.TWO: preload("res://assets/ui/player2.png"),
+		Character.PlayerIndex.THREE: preload("res://assets/ui/player3.png"),
+		Character.PlayerIndex.FOUR: preload("res://assets/ui/player4.png"),
+	}[player_index]
+	if player_index == Character.PlayerIndex.ONE:
+		blink()
+
+func blink() -> void:
+	if not is_instance_valid(character):
+		var tween := create_tween().set_loops(10)
+		tween.tween_property(%EmptySprite, "modulate:a", 0.0, 0.25).set_delay(0.5)
+		tween.tween_property(%EmptySprite, "modulate:a", 1.0, 0.25)
 
 
 func _on_player_joined(_character: Character) -> void:
 	if _character.player_index == player_index:
 		character = _character
-		%EmptyLabel.queue_free()
+		%EmptySprite.queue_free()
 		%Sprite.sprite_frames = character.character_type.sprite_frames
 		%Sprite.visible = true
+	elif _character.player_index == player_index - 1:
+		blink()
 
 func _on_player_died(_character: Character) -> void:
 	if _character == character:
