b308d8446370df42338be2d06605532b583f8b7e
Bertrand Bordage <bordage.bertrand@gmail.com>
2026-04-13 00:08:32 +0200

    Makes it possible to select 2D nodes despite the fade effect.

--- scenes/game.gd
+++ scenes/game.gd
@@ -7,3 +7,8 @@ func _ready() -> void:
 	fade.color.a = 1.0
 	var tween := create_tween()
 	tween.tween_property(fade, "color:a", 0.0, 3.0).set_ease(Tween.EASE_OUT)
+	tween.tween_callback(hide_fade)
+
+
+func hide_fade() -> void:
+	fade.visible = false
